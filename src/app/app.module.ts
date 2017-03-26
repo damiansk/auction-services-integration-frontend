b27ed9ec130332384b127d0735f392fbf69a985c
bumpMind <damian.s.ja@gmail.com>
2017-03-26 10:54:12 +0200

    Added SignInComponent to declarations in NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome';
+import { SignInComponent } from './welcome/signIn/signIn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
