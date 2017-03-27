5913abd9a823b7b04459883bd780a9df4b2c1cc3
bumpMind <damian.s.ja@gmail.com>
2017-03-27 09:09:42 +0200

    Added route from SignIn to SignUp

--- src/app/welcome/signIn/signIn.component.ts
+++ src/app/welcome/signIn/signIn.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
@@ -13,12 +14,17 @@ export class SignInComponent {
   });
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private router: Router
   ){}
 
   doSignIn(event): void {
     console.log(event);
     console.log(this.signInForm.value);
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['/register']);
   }
 
 }
