f46e96d89ae135db1c97d47ed2f93dfabfd335ae
bumpMind <damian.s.ja@gmail.com>
2017-03-27 17:34:33 +0200

    Added simple validators to SignUp

--- src/app/welcome/signUp/signUp.component.ts
+++ src/app/welcome/signUp/signUp.component.ts
@@ -1,21 +1,72 @@
-import { Component } from '@angular/core';
-import {ControlContainer, FormBuilder, FormControl, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
+import {runInThisContext} from "vm";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './signUp.component.html'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
 
+  private modelForm: FormGroup;
+  private formErrors = {
+    email: '',
+    password: '',
+    retypedPassword: ''
+  };
+  private validationMessages = {
+    email: {
+      required: 'To pole jest wymagane'
+    },
+    password: {
+      required: 'To pole jest wymagane',
+      minlength: 'Haslo musi sie skladac z co najmniej 6 znakow'
+    },
+    retypedPassword: {
+      required: 'To pole jest wymagane'
+    }
+  };
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router
   ) {}
 
-  doSignUp(): void {
+  ngOnInit(): void {
+    this.modelForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      retypedPassword: ['', Validators.required]
+    });
 
+    this.modelForm.valueChanges.subscribe( () => {
+      this.onControlValueChanged();
+    });
+
+    this.onControlValueChanged();
+  }
+
+  onSubmit(modelForm: FormGroup): void {
+    console.log(modelForm);
+  }
+
+  onControlValueChanged(): void {
+    const form = this.modelForm;
+
+    for (let field in this.formErrors) {
+      this.formErrors[field] = '';
+      let control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const validationMessages = this.validationMessages[field];
+
+        for (const key in control.errors) {
+          this.formErrors[field] += `${validationMessages[key]} `;
+        }
+      }
+
+    }
   }
 
   goToLogin(): void {
