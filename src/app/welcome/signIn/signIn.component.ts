1a35c63fcf41c0c67db002323b24938e57c6a9b3
bumpMind <damian.s.ja@gmail.com>
2017-03-28 12:55:25 +0200

    Changed FormGroup name

--- src/app/welcome/signIn/signIn.component.ts
+++ src/app/welcome/signIn/signIn.component.ts
@@ -1,26 +1,30 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './signIn.component.html'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
-  private signInForm = this.formBuilder.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
-  });
+  private modelForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router
-  ){}
+  ) {}
+
+  ngOnInit(): void {
+    this.modelForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   doSignIn(event): void {
     console.log(event);
-    console.log(this.signInForm.value);
+    console.log(this.modelForm.value);
   }
 
   goToRegister(): void {
