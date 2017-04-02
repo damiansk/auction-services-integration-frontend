7ac613de3b8e6c334c1222fefb53830fd8cf2d75
bumpMind <damian.s.ja@gmail.com>
2017-04-02 17:46:14 +0200

    Added simple implementation for register(User) function

--- src/app/welcome/signUp/sign-up.service.ts
+++ src/app/welcome/signUp/sign-up.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { environment } from '../../../environments/environment';
+
+import { User } from './sign-up.interface';
 
 @Injectable()
 export class SignUpService {
 
-  register(): void {
-    console.log( 'Service is here!' );
+  constructor( private http: Http ) {}
+
+  register(user: User): void {
+    this.http.post(`${environment.API_URL}/api/v1/user/register`, user)
+      .subscribe(
+        ( data ) => { console.log( `Data: ${data}`) },
+        ( error ) => { console.log( `Error: ${error}` ) },
+        () => { console.log( 'Complete' ) }
+      );
   }
 
 }
