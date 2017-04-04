e60cbd0712568cd1e9e984e11f6abd900a4390f1
bumpMind <damian.s.ja@gmail.com>
2017-04-04 14:58:06 +0200

    Fixed registerUser implementation

--- src/app/welcome/signUp/sign-up.service.ts
+++ src/app/welcome/signUp/sign-up.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 
 import { environment } from '../../../environments/environment';
 
 import { User } from './sign-up.interface';
-import {Observable} from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SignUpService {
 
-  private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({'Content-Type': 'application/json;charset=UTF-8;'});
 
   constructor( private http: Http ) {}
 
-  registerUser(user: User): Observable<string> {
-    return this.http
-                  .post(`${environment.API_URL}/api/v1/user/register`,
-                        JSON.stringify(user),
-                        {headers: this.headers})
-                  .map( response => response.json() );
+  registerUser(user: User) {
+    user.username = user.username || '';
+
+    this.http
+      .post( `${environment.API_URL}/api/v1/user/register`, JSON.stringify(user), {headers: this.headers} )
+      .subscribe(
+        data => console.log( `Data: ${data}` ),
+        err => console.error( `Err: ${err}` ),
+        () => console.log( 'Done' )
+      );
   }
 
 }
