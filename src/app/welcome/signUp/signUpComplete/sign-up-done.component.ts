4423d4b3c1a26cfe465b3af3f336199f85a68da6
bumpMind <damian.s.ja@gmail.com>
2017-04-05 09:52:53 +0200

    Added function to redirect user to Login page

--- src/app/welcome/signUp/signUpComplete/sign-up-done.component.ts
+++ src/app/welcome/signUp/signUpComplete/sign-up-done.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'done',
   template: `
     <h2>Twoja rejestracja przebiega pomyslnie!</h2>
     <p>Nacisnij ponizszy przycisk aby przejsc do strony logowania.</p>
-    <button>Dalej</button>
+    <button type="button" (click)="goToLogin()">Dalej</button>
   `
 })
 export class SignUpDoneComponent {
+
+  constructor(private router: Router) {}
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
 
 }
