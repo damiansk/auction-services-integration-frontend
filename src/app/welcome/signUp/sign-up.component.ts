560a9e17bd7c5f61a00517e265dee751868ce9f3
bumpMind <damian.s.ja@gmail.com>
2017-04-05 21:29:41 +0200

    Added register response interception

--- src/app/welcome/signUp/sign-up.component.ts
+++ src/app/welcome/signUp/sign-up.component.ts
@@ -54,7 +54,19 @@ export class SignUpComponent implements OnInit {
 
   onSubmit({value, valid}: {value: User, valid: boolean}): void {
     if ( valid === true ) {
-      this.signUpService.registerUser(value);
+      this.signUpService
+                .registerUser(value)
+                .subscribe(
+                    (data) => {
+                      if ( data.status === 201 ) {
+                        this.router.navigateByUrl('/register/done');
+                      } else {
+                        console.log('Wrong response status');
+                      }
+                    },
+                    (error) => {
+                      console.error(error);
+                    });
     }
   }
 
