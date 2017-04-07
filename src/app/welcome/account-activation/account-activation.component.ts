591515d4e0c678ca47e57f133e41b88fd62f2b32
bumpMind <damian.s.ja@gmail.com>
2017-04-07 18:22:38 +0200

    Added component for account activation requests

---
+++ src/app/welcome/account-activation/account-activation.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'account-activation',
+  template: '<h2>Account activation component</h2>'
+})
+export default class AccountActivationComonent {
+
+}
