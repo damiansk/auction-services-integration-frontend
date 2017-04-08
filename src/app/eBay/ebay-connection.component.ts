18ab3ad701f0c2c745f8f698d2c39d679846eb24
bumpMind <damian.s.ja@gmail.com>
2017-04-08 19:21:19 +0200

    EbayConnection component allows to redirect to the ebay connection page

--- src/app/eBay/ebay-connection.component.ts
+++ src/app/eBay/ebay-connection.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/map';
 
+import {environment} from '../../environments/environment';
+
+
 @Component({
   selector: 'ebay-connection',
-  template: '<a href="#">Connect Your Account</a>'
+  template: '<a href="{{link}}">Connect Your Account</a>'
 })
-export class EbayConnectionComponent {
+export class EbayConnectionComponent implements OnInit {
 
   private email = 'test@test.test';
+  private link = '#';
 
   constructor(public http: Http) {}
 
-  connectEbayAccount(): void {
+  ngOnInit(): void {
+    this.setActivationLink();
+  }
+
+  setActivationLink(): void {
+    this.http.get( `${environment.API_URL}/api/v1/ebay/auth/redirect/${this.email}`)
+      .subscribe(
+        data => this.link = data.text(),
+        err => console.error(err)
+      );
 
   }
 
