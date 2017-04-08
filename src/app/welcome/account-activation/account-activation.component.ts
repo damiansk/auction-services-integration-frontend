975a07cbc7aad75407f8604eab3c69d49985b69b
bumpMind <damian.s.ja@gmail.com>
2017-04-08 15:03:01 +0200

    Get token from request URL

--- src/app/welcome/account-activation/account-activation.component.ts
+++ src/app/welcome/account-activation/account-activation.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'account-activation',
-  template: '<h2>Account activation component</h2>'
+  template: '<h2>Account activation component</h2><p>{{token}}</p><p #waiting>Prosze czekać, trwa aktywowanie...</p>'
 })
-export class AccountActivationComponent {
+export class AccountActivationComponent implements OnInit {
 
-  constructor(private router: Router){}
+  private token: string;
+
+  constructor(private http: Http,
+              private router: Router,
+              private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.token = this.route.snapshot.queryParams['token'] || '';
+
+    //TODO Added activating request to server and handling response
+  }
 
 }
