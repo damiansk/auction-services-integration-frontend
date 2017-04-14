d2c8a43840e032b196b9200c57d331ae80291b8d
bumpMind <damian.s.ja@gmail.com>
2017-04-14 11:29:55 +0200

    Added eBay auth declined component

---
+++ src/app/ebay/auth/declined/ebay-auth-declined.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ebay-auth-declined',
+  template: '<h2>eBay auth declined</h2>'
+})
+export class EbayAuthDeclinedComponent {
+
+}
