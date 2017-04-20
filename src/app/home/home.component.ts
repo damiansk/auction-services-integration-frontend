d9a43798b7d8559815245bbe6b0ea5f3335ccc69
bumpMind <damian.s.ja@gmail.com>
2017-04-20 07:10:13 +0200

    Added a simple token check, if cannot find it then redirected to login page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const cookies = document.cookie.split("; ");
+    const tokenNameIndex = cookies.indexOf('Authorization');
+
+    if ( tokenNameIndex === -1 && cookies[tokenNameIndex+1].length !== 0 ) {
+      this.router.navigate([''], {relativeTo: this.route});
+    } else {
+      //TODO connect to server and check authorization token
+    }
+  }
+
+}
