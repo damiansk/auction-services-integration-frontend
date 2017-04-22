e403419767d2993d6d36f37cec5beb769efa038b
bumpMind <damian.s.ja@gmail.com>
2017-04-22 13:44:44 +0200

    ASI-56 Added welcome module

---
+++ src/app/welcome/welcome.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { WelcomeComponent } from './welcome.component';
+
+@NgModule({
+  declarations: [WelcomeComponent],
+  imports: [BrowserModule]
+})
+export class WelcomeModule {
+
+}
