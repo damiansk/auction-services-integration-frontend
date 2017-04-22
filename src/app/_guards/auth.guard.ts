9bdfb1ea38b4b381cc7c5c428a5b67972a0e17c5
bumpMind <damian.s.ja@gmail.com>
2017-04-22 18:39:44 +0200

    ASI-57 Updated canActivated() method with provider Auth service

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private authService: AuthService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    if ( this.authService.isAuthorized() ) {
+      return true;
+    } else {
+      this.router.navigate(['welcome/login']);
+      return false;
+    }
   }
 
 }
