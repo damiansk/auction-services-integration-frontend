003069ddb8dcc68987389e77bc879a3f4225e55d
bumpMind <damian.s.ja@gmail.com>
2017-04-22 15:19:44 +0200

    ASI-56 Added Home routing module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HomeRoutingModule
+  ],
   declarations: [HomeComponent]
 })
 export class HomeModule {
