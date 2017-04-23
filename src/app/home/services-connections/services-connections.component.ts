d4ddfba6ec31eb89244c2a9f11d68c3eee12d5e4
bumpMind <damian.s.ja@gmail.com>
2017-04-23 09:28:33 +0200

    ASI-33 Added Services Connections component

---
+++ src/app/home/services-connections/services-connections.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'services-connections',
+  templateUrl: './services-connections.component.html'
+})
+export class ServicesConnectionsComponent {
+
+  constructor() {}
+
+}
