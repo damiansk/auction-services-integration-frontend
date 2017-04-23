878d1c4ec32f4b0797bba42e473a4aa8952cadd2
bumpMind <damian.s.ja@gmail.com>
2017-04-23 12:28:57 +0200

    ASI-33 Added Ebay Auth Accepted component

---
+++ src/app/home/services-connections/ebay-auth/auth/accepted/ebay-auth-accepted.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ebay-auth-accepted',
+  template: '<h2>eBay auth complete!</h2>'
+})
+export class EbayAuthAcceptedComponent {
+
+  constructor() {}
+
+}
