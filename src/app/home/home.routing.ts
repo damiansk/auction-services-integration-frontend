d540307001785c5389c7c3dcfd36105552736ac8
bumpMind <damian.s.ja@gmail.com>
2017-04-23 12:34:41 +0200

    ASI-33 Removed unused components and routes

--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home.component';
-import { EbayConnectionComponent } from './ebay/ebay-connection.component';
-import { EbayAuthAcceptedComponent } from './ebay/auth/accepted/ebay-auth-accepted.component';
-import { EbayAuthDeclinedComponent } from './ebay/auth/declined/ebay-auth-declined.component';
 
 import { AuthGuard } from '../_guards/auth.guard';
 
@@ -12,12 +9,6 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard],
     children: [
       { path: 'services-connections', loadChildren: './services-connections/services-connections.module#ServicesConnectionsModule'}
-    ]
-  },
-  { path: 'ebay-connection', component: EbayConnectionComponent,
-    children: [
-      { path: 'auth-accepted', component: EbayAuthAcceptedComponent },
-      { path: 'auth-declined', component: EbayAuthDeclinedComponent }
     ]
   }
 ];
