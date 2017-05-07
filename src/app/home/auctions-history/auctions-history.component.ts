237e4883aff5ad4ff4f3492fc1b33d603d041db7
bumpMind <damian.s.ja@gmail.com>
2017-05-07 18:01:16 +0200

    ASI-71 Added Auctions history component with simple template

---
+++ src/app/home/auctions-history/auctions-history.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auctions-history',
+  templateUrl: 'auctions-history.component.html'
+})
+export class AuctionsHistoryComponent {}
