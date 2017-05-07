5674e6b8efaa032af4bc61368929917eef5fc691
bumpMind <damian.s.ja@gmail.com>
2017-05-07 18:04:01 +0200

    ASI-71 Added Auctions history module

---
+++ src/app/home/auctions-history/auctions-history.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AuctionsHistoryComponent } from './auctions-history.component';
+
+@NgModule({
+  declarations: [
+    AuctionsHistoryComponent
+  ],
+  imports: []
+})
+export class AuctionsHistoryModule {}
