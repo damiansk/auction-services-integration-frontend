151777e177452f470980d242f9d217735eddcd67
bumpMind <damian.s.ja@gmail.com>
2017-05-17 09:35:08 +0200

    ASI-33 Updated Allegro auth component

--- src/app/home/account/services-connections/allegro-auth/allegro-auth.component.ts
+++ src/app/home/account/services-connections/allegro-auth/allegro-auth.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ng2-cookies';
+
+import { AuthService } from '../../../../_services/auth.service';
+
+import { AllegroAuthService } from './allegro-auth.service';
+
 
 @Component({
   selector: 'allegro-auth',
@@ -7,19 +13,75 @@ import { Component, OnInit } from '@angular/core';
 export class AllegroAuthComponent implements OnInit {
 
   private expirationTime: string;
+  private isActive: boolean = false;
+  private statusColor: string = 'red';
 
-  constructor() {}
+  constructor(private authService: AuthService,
+              private cookieService: CookieService,
+              private allegroAuthService: AllegroAuthService) {}
 
   ngOnInit(): void {
-    this.getAccountExpirationTime();
+    this.updateAccountExpirationTime();
+  }
+
+  updateAccountExpirationTime(): void {
+    this.allegroAuthService
+      .getAccountExpirationTime()
+      .subscribe(
+        response => {
+          this.updateAuthToken(response.headers.get('authorization'));
+          this.updateAccountStatus(response.json());
+        },
+        (err) => {}
+      )
+
   }
 
   getActivationLink(): void {
+    this.allegroAuthService
+      .getActivationLink()
+      .subscribe(
+        response => {
+          this.updateAuthToken(response.headers.get('authorization'));
 
+          const responseBody = response.json();
+          this.cookieService.set('authToken', responseBody.authToken);
+          window.location.href = responseBody.allegroAuthUrl;
+        },
+        err => console.error( err )
+      );
   }
 
-  getAccountExpirationTime(): void {
+  private updateAuthToken(token: string) {
+    this.authService.setAuthToken(token);
+  }
 
+  private updateAccountStatus(status: any) {
+    if ( status.expirationDate ) {
+      this.expirationTime = status.expirationDate;
+      this.isActive = status.active;
+      this.statusColor = status.active ? 'green' : 'red';
+    }
+  }
+
+  getExpirationDate(): string {
+    return this.expirationTime ? this.allegroAuthService.decodeDate(new Date(this.expirationTime)) : '-';
+  }
+
+  public connectAccount(code: string): void {
+    const authToken = this.cookieService.get('authToken');
+    if ( code && authToken ) {
+      this.allegroAuthService
+        .connectAccount(code, authToken)
+        .subscribe(
+          response => {
+            this.updateAuthToken(response.headers.get('authorization'));
+            console.log('activated');
+            this.updateAccountExpirationTime();
+          },
+          err => console.error(err)
+        );
+    }
   }
 
 }
