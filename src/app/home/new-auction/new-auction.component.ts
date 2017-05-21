c28bc70b96bc86a6665deade07a3f5977a77864c
bumpMind <damian.s.ja@gmail.com>
2017-05-21 16:02:27 +0200

    ASI-55 Added simple instance of New Auction Module with mocked getCategoryParameters request to backend

--- src/app/home/new-auction/new-auction.component.ts
+++ src/app/home/new-auction/new-auction.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NewAuctionService } from './new-auction.service';
 
 @Component({
   selector: 'new-auction',
   templateUrl: './new-auction.component.html'
 })
-export class NewAuctionComponent {}
+export class NewAuctionComponent implements OnInit {
+
+  private fieldType: string[] = [
+    'STRING',
+    'INTEGER',
+    'FLOAT',
+    'IMAGE',
+    'DATETIME',
+    'DATE'
+  ];
+
+  private formType: string[] = [
+    'STRING',
+    'INTEGER',
+    'FLOAT',
+    'COMBOBOX',
+    'RADIOBUTTON',
+    'CHECKBOX',
+    'IMAGE',
+    'TEXTAREA',
+    'DATETIME',
+    'DATE'
+  ];
+
+  constructor(private newAuctionService: NewAuctionService) {}
+
+  ngOnInit() {
+    this.newAuctionService
+      .getCategoryParameters(20)
+      .subscribe(
+        (data) => console.log(data.json()),
+        (err) => console.error(err)
+      );
+  }
+
+}
--- src/app/home/new-auction/new-auction.module.ts
+++ src/app/home/new-auction/new-auction.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
 import { NewAuctionComponent } from './new-auction.component';
 import { NewAuctionRoutingModule } from './new-auction.routing';
+import { NewAuctionService } from './new-auction.service';
 
 @NgModule({
   declarations: [
     NewAuctionComponent
   ],
   imports: [
-    NewAuctionRoutingModule
+    NewAuctionRoutingModule,
+    HttpModule,
+    CommonModule
+  ],
+  providers: [
+    NewAuctionService
   ]
 })
 export class NewAuctionModule {}
