4784d687e5679275c800ecae829b0035ed77f4e4
bumpMind <damian.s.ja@gmail.com>
2017-05-28 15:32:42 +0200

    ASI-55 Added getCategoryList method to Allegro-category comp.

--- src/app/home/new-auction/allegro/allegro-category.service.ts
+++ src/app/home/new-auction/allegro/allegro-category.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
 import { AuthService } from '../../../_services/auth.service';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class AllegroCategoryService {
 
   constructor(private http: Http,
               private authService: AuthService) {}
+
+  getCategoryList(): Observable<Response> {
+    return this.http
+      .get(`${environment.API_URL}${environment.ALLEGRO_URL.getCategoryList}`,
+        {headers: new Headers( {
+          'Content-Type': 'application/json',
+          'Authorization': this.authService.getAuthToken()} )
+        });
+  }
+
 
 }
