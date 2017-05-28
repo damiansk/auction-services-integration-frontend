318e3103647817ac3d242617db205d54a168a4fb
bumpMind <damian.s.ja@gmail.com>
2017-05-28 15:24:13 +0200

    ASI-55 Added Allegro category component with empty template

---
+++ src/app/home/new-auction/allegro/allegro-category.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'allegro-category',
+  templateUrl: './allegro-category.component.html'
+})
+export class AllegroCategoryComponent {}
