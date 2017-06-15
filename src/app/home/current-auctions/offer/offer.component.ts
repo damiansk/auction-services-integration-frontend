65cb923f3ce76bf3cabbe88ed9e391b7c62dd72c
bumpMind <damian.s.ja@gmail.com>
2017-06-15 18:56:39 +0200

    Updatet some styles on show offers page

--- src/app/home/current-auctions/offer/offer.component.ts
+++ src/app/home/current-auctions/offer/offer.component.ts
@@ -3,7 +3,8 @@ import { Offer } from './offer.interface';
 
 @Component({
   selector: 'offer',
-  templateUrl: './offer.component.html'
+  templateUrl: './offer.component.html',
+  styleUrls: ['./offer.component.scss']
 })
 export class OfferComponent {
 
