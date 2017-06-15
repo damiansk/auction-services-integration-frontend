2e9113e2e1a135a96d6f83ea8b4dd8e75344b922
bumpMind <damian.s.ja@gmail.com>
2017-06-15 19:15:31 +0200

    Updated styles on password management component

--- src/app/home/account/account.routing.ts
+++ src/app/home/account/account.routing.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PasswordManagementComponent } from './password-management/password-management.component';
+import {ServicesConnectionsComponent} from './services-connections/services-connections.component';
 
 const routes: Routes = [
   //TODO Add not fount page
-  { path: '', redirectTo: 'services-connections' },
+  { path: 'services-connections', component: ServicesConnectionsComponent },
   { path: 'password-management', component: PasswordManagementComponent }
 ];
 
--- src/app/home/account/password-management/password-management.component.ts
+++ src/app/home/account/password-management/password-management.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'password-management',
-  templateUrl: 'password-management.component.html'
+  templateUrl: 'password-management.component.html',
+  styleUrls: ['./password-management.component.scss']
 })
 export class PasswordManagementComponent {}
