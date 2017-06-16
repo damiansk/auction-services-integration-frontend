cbeb1d4fcb2b6da339a812bbeb03d9b1ca56ee4e
bumpMind <damian.s.ja@gmail.com>
2017-06-16 05:56:59 +0200

    Updated contact component

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact',
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {}
