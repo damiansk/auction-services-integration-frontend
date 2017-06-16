9072474b001930891c2cbb3b65c42290537a6872
bumpMind <damian.s.ja@gmail.com>
2017-06-16 06:07:23 +0200

    Added eBay new auction component

---
+++ src/app/home/new-auction/ebay/ebay-new-auction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ebay-new-auction',
+  templateUrl: 'ebay-new-auction.component.html',
+  styleUrls: ['./ebay-new-auction.component.scss']
+})
+export class EbayNewAuctionComponent {
+
+}
--- src/app/home/new-auction/new-auction.module.ts
+++ src/app/home/new-auction/new-auction.module.ts
@@ -9,16 +9,18 @@ import { NewAuctionService } from './new-auction.service';
 
 import { AllegroCategoryComponent } from './allegro/allegro-category.component';
 import { AllegroCategoryService } from './allegro/allegro-category.service';
-import { EbayCategoryComponent } from './ebay/ebay-category.component';
-import { EbayCategoryService } from './ebay/ebay-category.service';
+import { EbayCategoryComponent } from './ebay/category/ebay-category.component';
+import { EbayCategoryService } from './ebay/category/ebay-category.service';
 import { AllegroCategoryAttributesService } from './allegro/category-attributes/allegro-category-attributes.service';
 import { AllegroCategoryAttributesComponent } from './allegro/category-attributes/allegro-category-attributes.component';
 import {EbayCategoryAttributesComponent} from './ebay/category-attributes/ebay-category-attributes.component';
 import {EbayCategoryAttributesService} from './ebay/category-attributes/ebay-category-attributes.service';
+import {EbayNewAuctionComponent} from './ebay/ebay-new-auction.component';
 
 @NgModule({
   declarations: [
     NewAuctionComponent,
+    EbayNewAuctionComponent,
     AllegroCategoryComponent,
     EbayCategoryComponent,
     AllegroCategoryAttributesComponent,
